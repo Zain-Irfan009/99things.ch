f7109678785b4735bcf5a54d15398187c7d58181
Zain-Irfan009 <zain.irfan@tetralogicx.com>
2023-11-16 19:42:44 +0500

    commit

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -64,16 +64,7 @@
                     </a>
                 </li>
 
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('products') ? 'active' : '' }}" href="{{route('products')}}" >
-                  <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/checkbox -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="9 11 12 14 20 6" /><path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" /></svg>
-                  </span>
-                        <span class="nav-link-title">
-                    Orders
-                  </span>
-                    </a>
-                </li>
+
 
                 <li class="nav-item">
                     <a class="nav-link {{ request()->routeIs('logs') ? 'active' : '' }}" href="{{route('logs')}}" >
